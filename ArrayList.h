949995602733f9a3638ca4b8ebfd21e89985444e
Michał Łachut <michallachut4@gmail.com>
2025-04-05 20:22:03 +0200

    array test commit początek

--- ArrayList.h
+++ ArrayList.h
@@ -1,7 +1,26 @@
 #pragma once
 #include "IList.h"
+#incude<iostream>
 
-class ArrayList:public IList
+template <typename T>
+class ArrayList:public IList<T>
 {
+private:
+	T* array;
+	int capacity;
+	int current_size;
+	void resize() {
+		capacity *= 2;
+		T* newArray = new T[capacity];
+		for (int i = 0; i < current_size; i++); {
+			newArray[i] = array[i];
+		}
+	}
+public:
+	ArrayList() : capacity(10), current_size(0) {
+		array = new T[capacity];
+	}
+
 };
+
 
--- MiniProjekt1SD.cpp
+++ MiniProjekt1SD.cpp
@@ -5,5 +5,6 @@
 
 int main()
 {
-
+	std::cout << "Hello world";
+	return 0;
 }
