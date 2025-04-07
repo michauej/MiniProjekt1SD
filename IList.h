4508c88744787f9875faff2dadf7e65b876769fd
Michał Łachut <michallachut4@gmail.com>
2025-04-07 09:20:18 +0200

    IList

--- IList.h
+++ IList.h
@@ -1,4 +1,15 @@
 #pragma once
 
+template <typename T>
 class IList {
+public:
+    virtual void addAtStart(T value) = 0;
+    virtual void addAtEnd(T value) = 0;
+    virtual void addAtIndex(int index, T value) = 0;
+    virtual void removeAtStart() = 0;
+    virtual void removeAtEnd() = 0;
+    virtual void removeAtIndex(int index) = 0;
+    virtual bool search(T value) = 0;
+    virtual int getSize() = 0;
+    virtual ~IList() {}
 };
